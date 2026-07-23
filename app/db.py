89fdd57a3a4a8bf277685f522b9c6ada1ada21a6
Sourav Kumar Paul <theskpaul@proton.me>
2026-07-23 10:12:55 +0530

    Change name of memory database to chat_history
    Signed-off-by: Sourav Kumar Paul <theskpaul@proton.me>

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 
 BASE_DIR = os.path.dirname(__file__)
-DB_PATH = os.path.join(BASE_DIR, "../data/memory.db")
+DB_PATH = os.path.join(BASE_DIR, "../data/chat_history.db")
 
 
 def get_connection():
