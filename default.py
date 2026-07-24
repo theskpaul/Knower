61f9340dfdf1fba6a60d5f7bae77d177ebb8a0fa
Sourav Kumar Paul <theskpaul@proton.me>
2026-07-25 01:21:48 +0530

    Add default.py to define predefined PATHs, configs

---
+++ default.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+PATH = {
+    "config": BASE_DIR / "config",
+    "temp_config": BASE_DIR / "data" / "config",
+    "chat_history": BASE_DIR / "data",
+    "sources": BASE_DIR / "data" / "sources",
+    "vectordb": BASE_DIR / "data" / "vectordb",
+    "log": BASE_DIR / "data" / "log",
+}
+
+DEFAULT_CONFIG = {
+    "LANGUAGE_MODEL": "hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF:latest",
+    "EMBEDDING_MODEL": "hf.co/CompendiumLabs/bge-base-en-v1.5-gguf:latest",
+}
+
+
+def check_and_make():
+    for p in PATH.values():
+        p.mkdir(parents=True, exist_ok=True)
