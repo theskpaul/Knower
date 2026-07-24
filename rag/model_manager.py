5dbd6f1f3578d64ac056c58c3fd7c1494148d766
Sourav Kumar Paul <theskpaul@proton.me>
2026-07-25 01:41:42 +0530

    Add ability to get installed model list from ollama Remove dependence on manual model name entry, now it will try to load model names from either default config or locally defined confi
    Signed-off-by: Sourav Kumar Paul <theskpaul@proton.me>

--- rag/model_manager.py
+++ rag/model_manager.py
@@ -1,18 +1,45 @@
+import requests
 from langchain_ollama import OllamaEmbeddings, OllamaLLM
 
+from default import DEFAULT_CONFIG as DC
+from helper.config import load_config
+from helper.file_record import Model
 from helper.logger import log
 
 
 class ModelManager:
-    def __init__(self, llm="", embedding_model="") -> None:
-        self.large_language_model = llm
-        self.embedding_model = embedding_model
+    def __init__(self) -> None:
+        load = load_config()
+        llm = load["LANGUAGE_MODEL"]
+        em = load["EMBEDDING_MODEL"]
 
-    @log("Give a prompt to the LLM")
-    def ask(self, input: str, temperature: float, num_ctx:int = 4096):
-        return OllamaLLM(
-            model=self.large_language_model, temperature=temperature, num_ctx=num_ctx
-        ).invoke(input)
+        self.large_language_model = llm if llm else DC["LANGUAGE_MODEL"]
+        self.embedding_model = em if em else DC["EMBEDDING_MODEL"]
+
+    def getOllamaModelList(self):
+        OLLAMA_URL = "http://localhost:11434"
+        req = requests.get(OLLAMA_URL + "/api/tags")
+        output = req.json()
+
+        result = []
+        for item in output["models"]:
+            result.append(
+                Model(
+                    name=item["name"],
+                    model=item["model"],
+                    size=item["size"],
+                    capabilities=item["capabilities"],
+                    details=item["details"],
+                )
+            )
+
+        return result
 
     def getEmbedder(self):
         return OllamaEmbeddings(model=self.embedding_model)
+
+    @log("Give a prompt to the LLM")
+    def ask(self, input: str, temperature: float, num_ctx: int = 4096):
+        return OllamaLLM(
+            model=self.large_language_model, temperature=temperature, num_ctx=num_ctx
+        ).invoke(input)
