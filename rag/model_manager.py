70a9dff04f573afb50646b5a70f194523b281dee
Sourav Kumar Paul <theskpaul@proton.me>
2026-07-25 23:07:06 +0530

    Move getOllamaModelList function out of the ModelManager class ModelManager will take LANGUAGE_MODEL and EMBEDDING_MODEL as Class input and there will not fallback

--- rag/model_manager.py
+++ rag/model_manager.py
@@ -1,39 +1,38 @@
 import requests
 from langchain_ollama import OllamaEmbeddings, OllamaLLM
 
-from default import DEFAULT_CONFIG as DC
-from helper.config import load_config
 from helper.file_record import Model
 from helper.logger import log
 
 
-class ModelManager:
-    def __init__(self) -> None:
-        load: dict[str, str] = load_config()
-        llm = load.get("LANGUAGE_MODEL")
-        em = load.get("EMBEDDING_MODEL")
+def getOllamaModelList():
+    OLLAMA_URL = "http://localhost:11434"
+    req = requests.get(OLLAMA_URL + "/api/tags")
+    output = req.json()
 
-        self.large_language_model = llm if llm else DC["LANGUAGE_MODEL"]
-        self.embedding_model = em if em else DC["EMBEDDING_MODEL"]
-
-    def getOllamaModelList(self):
-        OLLAMA_URL = "http://localhost:11434"
-        req = requests.get(OLLAMA_URL + "/api/tags")
-        output = req.json()
-
-        result = []
-        for item in output["models"]:
-            result.append(
-                Model(
-                    name=item["name"],
-                    model=item["model"],
-                    size=item["size"],
-                    capabilities=item["capabilities"],
-                    details=item["details"],
-                )
+    result = []
+    for item in output["models"]:
+        result.append(
+            Model(
+                name=item["name"],
+                model=item["model"],
+                size=item["size"],
+                capabilities=item["capabilities"],
+                details=item["details"],
             )
+        )
 
-        return result
+    return result
+
+
+class ModelManager:
+    def __init__(
+        self,
+        language_model: str,
+        embedding_model: str,
+    ) -> None:
+        self.large_language_model = language_model
+        self.embedding_model = embedding_model
 
     def getEmbedder(self):
         return OllamaEmbeddings(model=self.embedding_model)
